4ce78d755119b6a8d64753418e70b20860910354
cchengwen <1094848143@qq.com>
2020-12-06 10:28:51 +0800

    前台项目压缩包、解压后的项目

---
+++ zjxccloud/src/main/java/com/zjxccloud/ZjxccloudApplication.java
@@ -0,0 +1,13 @@
+package com.zjxccloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjxccloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjxccloudApplication.class, args);
+    }
+
+}
---
+++ zjxccloud/src/test/java/com/zjxccloud/ZjxccloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjxccloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZjxccloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
